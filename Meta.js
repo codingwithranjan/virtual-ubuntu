38283227f99ba531b4acdda5fcd1b8f35994e15f
Avnish Ranjan <112241781+codingwithranjan@users.noreply.github.com>
2024-12-17 13:49:40 +0530

    Add files via upload

---
+++ Meta.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Team Avnish-First Major Project</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Team Avnish-First Major Project" />
+            <meta name="description"
+                content="Team Avnish-First Major Project. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Avnish Ranjan (avnish)" />
+            <meta name="keywords"
+                content="avnish, avnish's portfolio, avnish linux, ubuntu , avnish ranjan ,avnish ranjan computer, avnish ranjan, avnish ubuntu, avnish ranjan ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Team Avnish-First Major Project" />
+            <meta itemProp="description"
+                content="Team Avnish-First Major Project. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Team Avnish-First Major Project" />
+            <meta name="twitter:description"
+                content="Team Avnish-First Major Project. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="avnish" />
+            <meta name="twitter:creator" content="avnish" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Team Avnish-First Major Project" />
+            <meta name="og:description"
+                content="Team Avnish-First Major Project. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.svg" />
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
